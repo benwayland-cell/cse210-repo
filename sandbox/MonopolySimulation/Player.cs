8e24230cc488c0773e9210721d78a1fb47e1a2ec
benwayland-cell <ben.wayland2007@gmail.com>
2025-11-26 11:10:21 -0700

    Added bones for the Player class

--- sandbox/MonopolySimulation/Player.cs
+++ sandbox/MonopolySimulation/Player.cs
@@ -1,11 +1,39 @@
 
 public class Player
 {
+    private string name;
     private int location = 0;
     private int money = 1500;
     private List<Property> ownedProperties = new List<Property>();
     private int turnsInJail = 0;
     private int numOfGetOutOfJailCards = 0;
+
+    public Player(string _name)
+    {
+        name = _name;
+    }
+
+    public void UpdateMoney(int amountToChange)
+    {
+        money += amountToChange;
+    }
+
+    public void GetJailCard()
+    {
+        numOfGetOutOfJailCards ++;
+    }
+    
+    public void AddProperty(Property propertyToAdd)
+    {
+        ownedProperties.Add(propertyToAdd);
+    }
+
+    // public void RemoveProperty()
+    
+    public void RunTurn()
+    {
+        Console.WriteLine($"Run {name}'s turn");
+    }
 
 
 }
