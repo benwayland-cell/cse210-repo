d92e76baf0a0da5920e3c6c4c3f83f0ac66d74cc
benwayland-cell <ben.wayland2007@gmail.com>
2025-11-30 18:50:08 -0700

    Some work on writing the User Interface.

--- sandbox/MonopolySimulation/Program.cs
+++ sandbox/MonopolySimulation/Program.cs
@@ -6,16 +6,16 @@ class Program
         // Property testProperty = new StandardProperty("test name", 100, "test color", 3);
         // Console.WriteLine(testProperty.GetName());
 
-        Player testPlayer = new Player("test player name");
+        // Player testPlayer = new Player("test player name");
 
         // testProperty.LandOnSpace(testPlayer);
 
-        Console.WriteLine(testPlayer.GetName());
-        Console.WriteLine(testPlayer.GetMoney());
-        testPlayer.UpdateMoney(-100);
-        Console.WriteLine(testPlayer.GetMoney());
+        // Console.WriteLine(testPlayer.GetName());
+        // Console.WriteLine(testPlayer.GetMoney());
+        // testPlayer.UpdateMoney(-100);
+        // Console.WriteLine(testPlayer.GetMoney());
 
-
+        
 
     }
 }
---
+++ sandbox/MonopolySimulation/UserInterface.cs
@@ -0,0 +1,26 @@
+
+using System.Reflection.Metadata;
+
+public class UserInterface
+{
+    private static List<Space> board;
+    private static Player[] playerList;
+    // private List<Card> chanceDeck
+    // private List<Card> communityChestDeck
+
+    static public void MainLoop(Player[] _playerList)
+    {
+        // init data
+        playerList = _playerList;
+        // List<Card> chanceDeck = Card.GetChanceDeck()
+        // List<Card> communityChestDeck = Card.GetCommunityChestDeck()
+
+        
+    }
+
+    static private void DisplayBoard()
+    {
+        
+    }
+    
+}
