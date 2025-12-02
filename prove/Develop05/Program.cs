0d5a0552c39fbf1ea8e1c9f16227e851517db594
benwayland-cell <ben.wayland2007@gmail.com>
2025-12-02 13:31:22 -0700

    Removed a few unnecessary commetns.

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -5,8 +5,5 @@ class Program
     static void Main(string[] args)
     {
         UserInterface.RunGoalProgram();
-
-        // Goal testSimpleGoal = new SimpleGoal("test name", "test description", 100);
-        // testSimpleGoal.Display();
     }
 }
