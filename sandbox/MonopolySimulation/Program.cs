507fb17f11fdf87d61a2ea3d5bf3606615230e69
benwayland-cell <ben.wayland2007@gmail.com>
2025-12-03 10:10:57 -0700

    Changing Program so it is in a "functional" state.

--- sandbox/MonopolySimulation/Program.cs
+++ sandbox/MonopolySimulation/Program.cs
@@ -3,33 +3,35 @@ class Program
 {
     static void Main()
     {
-        // Property testProperty = new StandardProperty("test name", 100, "test color", 3);
-        // Console.WriteLine(testProperty.GetName());
+        Property testProperty = new StandardProperty("test name", 100, "test color", 3);
+        Console.WriteLine(testProperty.GetName());
+        Console.WriteLine();
 
-        // Player testPlayer = new Player("test player name");
+        Player testPlayer = new Player("test player name");
 
-        // testProperty.LandOnSpace(testPlayer);
+        testProperty.LandOnSpace(testPlayer);
 
-        // Console.WriteLine(testPlayer.GetName());
-        // Console.WriteLine(testPlayer.GetMoney());
-        // testPlayer.UpdateMoney(-100);
-        // Console.WriteLine(testPlayer.GetMoney());
+        Console.WriteLine(testPlayer.GetName());
+        Console.WriteLine(testPlayer.GetMoney());
+        testPlayer.UpdateMoney(-100);
+        Console.WriteLine(testPlayer.GetMoney());
+        Console.WriteLine();
 
-        // Card testCard1 = new Card("Test text 1", 0, 10);
-        // testCard1.RunCard(testPlayer);
+        Card testCard1 = new Card("Test text 1", 0, 10);
+        testCard1.PlayCard(testPlayer);
 
-        // Card testCard2 = new Card("Test text 2", 1, 10);
-        // testCard2.RunCard(testPlayer);
+        Card testCard2 = new Card("Test text 2", 1, 10);
+        testCard2.PlayCard(testPlayer);
 
-        // Card testCard3 = new Card("Test text 3", 2, 10);
-        // testCard3.RunCard(testPlayer);
+        Card testCard3 = new Card("Test text 3", 2, 10);
+        testCard3.PlayCard(testPlayer);
+        Console.WriteLine();
 
-        // Space[] testBoard = Space.GetBoard();
+        Space[] testBoard = Space.GetBoard();
 
-        // testPlayer.Display();
-        // testPlayer.MoveToRelative(1);
-        // testPlayer.Display();
-        // testPlayer.MoveToAbsolute(0);
-        // testPlayer.Display();
+
+        testPlayer.Display();
+        testPlayer.MoveToRelative(1);
+        testPlayer.Display();
     }
 }
