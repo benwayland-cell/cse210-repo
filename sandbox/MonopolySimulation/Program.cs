d2394a02871d445ad6b7fbafaace8ced58f9d22e
benwayland-cell <ben.wayland2007@gmail.com>
2025-12-13 12:15:25 -0700

    Added making your own names for the game

--- sandbox/MonopolySimulation/Program.cs
+++ sandbox/MonopolySimulation/Program.cs
@@ -3,14 +3,36 @@ class Program
 {
     static void Main()
     {
-        List<Player> testPlayerArray = new List<Player>
-        {
-            new Player("Test name 1"),
-            new Player("Test name 2"),
-            new Player("Test name 3"),
-            new Player("Test name 4")
-        };
+        
+        List<Player> playerList= new List<Player>();
 
-        UserInterface.MainLoop(testPlayerArray);
+        string ? userInput;
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("CurrentPlayers:");
+            // display the list of players
+            foreach (Player player in playerList)
+            {
+                Console.WriteLine(player.GetName());
+            }
+
+            // get the name of the next player
+            Console.WriteLine("\nWhat is the name of the next player? (\"done\" if done)");
+            userInput = Console.ReadLine();
+
+            // check for null and "done"
+            if (userInput is null) userInput = "done";
+            if (userInput == "done") break;
+
+            // Add the player to the list
+            playerList.Add(new Player(userInput));
+            Console.WriteLine();
+
+        }
+
+        Console.WriteLine("Playing game");
+
+        UserInterface.MainLoop(playerList);
     }
 }
